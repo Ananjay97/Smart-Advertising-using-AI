344119ff100c395995e6ae663f96730ab37e9cc6
Ananjay <70649645+Ananjay97@users.noreply.github.com>
2020-09-03 13:11:01 +0530

    Create timer.py

---
+++ timer.py
@@ -0,0 +1,28 @@
+from subprocess import call
+
+import time
+import os
+
+from apscheduler.schedulers.background import BackgroundScheduler
+from pytz import utc
+
+
+def job():
+    print("LAUNCHING DETECTION")
+    call(['python', 'C:/Users/ananj/PycharmProjects/Clg/main.py'])
+
+
+if __name__ == '__main__':
+    scheduler = BackgroundScheduler()
+    scheduler.configure(timezone=utc)
+    scheduler.add_job(job, 'interval', seconds=2)
+    scheduler.start()
+    print('Press Ctrl+{0} to exit'.format('Break' if os.name == 'nt' else 'C'))
+
+    try:
+        # This is here to simulate application activity (which keeps the main thread alive).
+        while True:
+            time.sleep(5)
+    except (KeyboardInterrupt, SystemExit):
+        # Not strictly necessary if daemonic mode is enabled but should be done if possible
+        scheduler.shutdown()
